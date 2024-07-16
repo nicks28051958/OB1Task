b341847d97bd94509e5c3f36eda07229665386b9
nicks28051958 <semchenko.ni@gmail.com>
2024-07-16 12:31:58 +0500

    change 3 main

--- main.py
+++ main.py
@@ -21,23 +21,23 @@ class TaskManagerApp:
 
         self.tasks = []
 
-        self.description_label = tk.Label(root, text="Description:", bg="light gray")
+        self.description_label = tk.Label(root, text="Описание задачи:", bg="light gray")
         self.description_label.grid(row=0, column=0)
         self.description_entry = tk.Entry(root, width=50)
         self.description_entry.grid(row=0, column=1, columnspan=2)
 
-        self.due_date_label = tk.Label(root, text="Due Date (DD-MM-YYYY):", bg="light gray")
+        self.due_date_label = tk.Label(root, text="Введите контрольную дату в формате (DD-MM-YYYY):", bg="light gray")
         self.due_date_label.grid(row=1, column=0)
         self.due_date_entry = tk.Entry(root, width=50)
         self.due_date_entry.grid(row=1, column=1, columnspan=2)
 
-        self.add_button = tk.Button(root, text="Add Task", command=self.add_task)
+        self.add_button = tk.Button(root, text="Добавить Задачу", command=self.add_task)
         self.add_button.grid(row=2, column=0, columnspan=3)
 
         self.tasks_listbox = tk.Listbox(root, width=50, height=15)
         self.tasks_listbox.grid(row=3, column=0, columnspan=2)
 
-        self.mark_completed_button = tk.Button(root, text="Mark as Completed", command=self.mark_completed)
+        self.mark_completed_button = tk.Button(root, text="Задача выполнена", command=self.mark_completed)
         self.mark_completed_button.grid(row=3, column=2)
 
         self.refresh_tasks()
@@ -49,7 +49,7 @@ class TaskManagerApp:
         try:
             due_date = datetime.strptime(due_date_str, '%d-%m-%Y')
         except ValueError:
-            messagebox.showerror("Error", "Due date must be in DD-MM-YYYY format")
+            messagebox.showerror("Error", "Дата должна быть в формате DD-MM-YYYY")
             return
 
         task = Task(description, due_date)
@@ -59,7 +59,7 @@ class TaskManagerApp:
     def mark_completed(self):
         selected_index = self.tasks_listbox.curselection()
         if not selected_index:
-            messagebox.showerror("Error", "No task selected")
+            messagebox.showerror("Error", "Не выбрано ни одной задачи")
             return
 
         task_index = selected_index[0]
@@ -70,7 +70,7 @@ class TaskManagerApp:
         self.tasks_listbox.delete(0, tk.END)
         for task in self.tasks:
             if not task.completed:
-                task_str = f"{task.description} (Due: {task.due_date.strftime('%d-%m-%Y')})"
+                task_str = f"{task.description} (Дата завершения: {task.due_date.strftime('%d-%m-%Y')})"
                 self.tasks_listbox.insert(tk.END, task_str)
 
 
